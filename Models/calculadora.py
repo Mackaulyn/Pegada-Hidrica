7da71211665439511385b9d65977b78aa535c845
Mackaulyn <mackaulynrocha@gmail.com>
2025-10-24 18:24:32 -0300

    Update calculadora.py

--- Models/calculadora.py
+++ Models/calculadora.py
@@ -4,14 +4,19 @@ class Pegadahidrica:
         self.roupas = 0
         self.torneira = 0
         self.familiares = 0
+        
     def get_banho(self):
         return self.banho
+        
     def get_roupas(self):
         return self.roupas    
+        
     def get_torneira(self):
         return self.torneira
+        
     def set_familiares(self):
         self.familiares = int(input("Digite o número de pessoas na sua família: "))
+        
     def set_banho(self):
     def set_roupas(self):
     def set_torneira(self):
